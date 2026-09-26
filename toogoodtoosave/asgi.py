1470089034fae31171274f0c7a98f7b6fe6a0f18
“shruti” <shruti.is.bored@gmail.com>
2026-09-26 17:36:39 +0530

    added captions

--- toogoodtoosave/asgi.py
+++ toogoodtoosave/asgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 """
 
-import os
 
-from django.core.asgi import get_asgi_application
+#ASGI = Asynchronous Server Gateway Interface
+import os #Python's built-in module for working with the operating system
+
+from django.core.asgi import get_asgi_application #a function provided by django 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toogoodtoosave.settings')
+#If the Django settings module hasn't already been set, use toogoodtosave.settings as the default.
 
 application = get_asgi_application()
+#creates djangos ASGI application ->
+#the ASGI server looks for this application object to communicate with your Django project.
